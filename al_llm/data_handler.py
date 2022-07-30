0c198a359ef371b76cd3a35d21424bca892e789c
Louis Thomson <louis@cbthomson.com>
2022-07-30 15:48:17 +0100

    Starting implementing HuggingFaceDataHandler class

--- al_llm/data_handler.py
+++ al_llm/data_handler.py
@@ -24,9 +24,15 @@ class DataHandler(ABC):
 
     Attributes
     ----------
-    dataset : dataset.Dataset
-        The raw dataset consisting of labelled sentences, as a Hugging Face
-        Dataset.
+    dataset_train : datasets.Dataset
+        The raw dataset consisting of labelled sentences used for training, as
+        a Hugging Face Dataset.
+    dataset_validation : datasets.Dataset
+        The raw dataset consisting of labelled sentences used for validation, as
+        a Hugging Face Dataset.
+    dataset_test : datasets.Dataset
+        The raw dataset consisting of labelled sentences used for testing, as
+        a Hugging Face Dataset.
     tokenized_dataset : torch.utils.data.Dataset
         The tokenized dataset, as a PyTorch dataset.
     classifier : classifier.Classifier
@@ -93,20 +99,66 @@ class HuggingFaceDataHandler(DataHandler):
     classifier : classifier.Classifier
         The classifier instance which will be using the data. We will use this
         to know how to tokenize the data.
+    validation_proportion : float, default=0.2
+        Proportion of the training data to be used for validation, if it's not
+        provided by the Hugging Face dataset.
 
     Attributes
     ----------
-    dataset : dataset.Dataset
-        The raw dataset consisting of labelled sentences, as a Hugging Face
-        Dataset.
+    dataset_train : datasets.Dataset
+        The raw dataset consisting of labelled sentences used for training, as
+        a Hugging Face Dataset.
+    dataset_validation : datasets.Dataset
+        The raw dataset consisting of labelled sentences used for validation, as
+        a Hugging Face Dataset.
+    dataset_test : datasets.Dataset
+        The raw dataset consisting of labelled sentences used for testing, as
+        a Hugging Face Dataset.
     tokenized_dataset : torch.utils.data.Dataset
         The tokenized dataset, as a PyTorch dataset.
     classifier : classifier.Classifier
         The classifier instance which will be using the data.
     """
 
-    def __init__(self, dataset_name, classifier, parameters):
-        pass
+    def __init__(self, dataset_name, classifier, parameters, validation_proportion=0.2):
+
+        # get dataset split names and ensure contains only train, test, or
+        # validation
+        split_names = datasets.get_dataset_split_names(dataset_name)
+        for name in split_names:
+            assert name in ["train", "test", "validation"]
+
+        # load the training dataset from Hugging Face
+        dataset_train_split = datasets.load_dataset(dataset_name, split="train")
+
+        # if the Hugging Face dataset provides a validation set, load it
+        if "validation" in split_names:
+            self.dataset_validation = datasets.load_dataset(
+                dataset_name,
+                split="validation",
+            )
+            self.dataset_train = dataset_train_split
+
+        # otherwise, use the `validation_proportion` to take some of the
+        # training set for validation
+        else:
+            train_length = len(dataset_train_split)
+            validation_length = train_length * validation_proportion
+
+            # shuffle the training set before splitting it
+            dataset_train_split = dataset_train_split.shuffle()
+
+            # take the last `validation_proportion` elements for validation,
+            # and use the remaining elements for training
+            self.dataset_validation = dataset_train_split[-validation_length:]
+            self.dataset_train = dataset_train_split[:-validation_length]
+
+        # load the testing dataset from Hugging Face
+        self.dataset_test = datasets.load_dataset(dataset_name, split="test")
+
+        self.tokenized_dataset = None
+        self.classifier = classifier
+        self.parameters = parameters
 
     def new_labelled(self, samples, labels):
         return TensorDataset(torch.rand(100, 100))
