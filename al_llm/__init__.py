c5796433444ff862930e7d5099398c5f9d7bda04
Louis Thomson <louis@cbthomson.com>
2022-08-06 17:09:28 +0100

    added new concrete classes to 'init.py' file

--- al_llm/__init__.py
+++ al_llm/__init__.py
@@ -1,5 +1,5 @@
-from .classifier import Classifier, DummyClassifier
-from .data_handler import DataHandler, DummyDataHandler
+from .classifier import Classifier, DummyClassifier, GPT2Classifier
+from .data_handler import DataHandler, DummyDataHandler, HuggingFaceDataHandler
 from .experiment import Experiment
 from .interface import Interface, CLIInterface
-from .sample_generator import SampleGenerator, DummySampleGenerator
+from .sample_generator import SampleGenerator, DummySampleGenerator, PlainGPT2SampleGenerator
