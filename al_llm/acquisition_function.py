da9ceeefa58aab0880827e51f1496bcaea5c8e92
Sam Adam-Day <me@samadamday.com>
2022-08-08 21:20:11 +0200

    Implemented RandomAcquisitionFunction

--- al_llm/acquisition_function.py
+++ al_llm/acquisition_function.py
@@ -2,6 +2,7 @@
 # https://docs.python.org/3.10/library/abc.html
 from abc import ABC, abstractmethod
 
+import random
 
 class AcquisitionFunction(ABC):
     """Base acquisition function
@@ -37,7 +38,7 @@ class AcquisitionFunction(ABC):
 
 
 class DummyAcquisitionFunction(AcquisitionFunction):
-    """A dummy acquisition function, which select the first slice of samples
+    """A dummy acquisition function, which selects the first slice of samples
 
     Parameters
     ----------
@@ -69,3 +70,38 @@ class DummyAcquisitionFunction(AcquisitionFunction):
             raise ValueError("Size of `samples` is smaller than `num_samples`")
 
         return samples[:num_samples]
+
+
+class RandomAcquisitionFunction(AcquisitionFunction):
+    """An acquisition function which select randomly
+
+    Parameters
+    ----------
+    parameters : dict
+        The dictionary of parameters for the present experiment
+    """
+
+    def select(self, samples: list, num_samples: int = -1) -> list:
+        """Apply the acquisition function
+
+        Parameters
+        ----------
+        samples : list
+            The list of sentences from which to sample
+        num_samples : int, default=-1
+            The number of samples to select. The default value of -1 means
+            that `parameters["num_samples"]` is used.
+
+        Returns
+        -------
+        selected_samples : list
+            A sublist of `samples` of size `num_samples` selected according
+            the to acquisition function.
+        """
+        if num_samples == -1:
+            num_samples = self.parameters["num_samples"]
+
+        if num_samples > len(samples):
+            raise ValueError("Size of `samples` is smaller than `num_samples`")
+
+        return random.sample(samples, num_samples)
