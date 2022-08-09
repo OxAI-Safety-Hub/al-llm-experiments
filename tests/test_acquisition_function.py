39120ec4664389f299f2cc3b26d61e35f9997510
Sam Adam-Day <me@samadamday.com>
2022-08-09 22:33:43 +0200

    More comprehensive test of uncertainty sampler

--- tests/test_acquisition_function.py
+++ tests/test_acquisition_function.py
@@ -1,9 +1,18 @@
+import random
+
 from al_llm.acquisition_function import (
     DummyAcquisitionFunction,
     RandomAcquisitionFunction,
     MaxUncertaintyAcquisitionFunction,
 )
 from al_llm.classifier import DummyClassifier
+
+
+class LengthUncertaintyClassifier(DummyClassifier):
+    """A classifier where the uncertainty is the length of the sentence"""
+
+    def calculate_uncertainties(self, samples: list) -> list:
+        return [len(sample) for sample in samples]
 
 
 def _basic_acquisition_function_test(acquisition_function_cls):
@@ -39,4 +48,28 @@ def test_dummy_acquisition_function():
 
 
 def test_max_uncertainty_function():
+
+    # Do a basic test with this sampler
     _basic_acquisition_function_test(MaxUncertaintyAcquisitionFunction)
+
+    # Some basic parameters
+    num_samples = 5
+    num_oversamples = 20
+    parameters = {"num_samples": num_samples, "num_oversamples": num_oversamples}
+
+    # Set up the acquisition function
+    classifier = LengthUncertaintyClassifier(parameters)
+    acquisition_function = MaxUncertaintyAcquisitionFunction(parameters, classifier)
+
+    # Generate some samples with increasing length
+    samples = ["a" * i for i in range(num_oversamples)]
+
+    # Shuffle these to make it harder
+    random.seed(3535)
+    random.shuffle(samples)
+
+    # The selected samples should be the longest ones
+    target_samples = [
+        "a" * i for i in range(num_oversamples - num_samples, num_oversamples)
+    ]
+    assert acquisition_function.select(samples) == target_samples
