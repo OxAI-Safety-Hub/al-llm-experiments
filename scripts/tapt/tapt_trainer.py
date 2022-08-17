4a0a5cebb93874afda1ae253e7655e79c17db0d3
Sam Dower <samdower@hotmail.co.uk>
2022-08-17 19:27:24 +0100

    Formatted with black

--- scripts/tapt/tapt_trainer.py
+++ scripts/tapt/tapt_trainer.py
@@ -2,22 +2,40 @@ import os
 from venv import create
 import argparse
 
-parser = argparse.ArgumentParser(description="Pretrain the model on the unlabelled data.")
+parser = argparse.ArgumentParser(
+    description="Pretrain the model on the unlabelled data."
+)
 parser.add_argument("--model-name", type=str, help="The hugging face model name.")
 parser.add_argument("--dataset-name", type=str, help="The dataset name or path.")
-parser.add_argument("--batch-size", type=int, help="The batch size for training.", default=8, nargs="?")
+parser.add_argument(
+    "--batch-size", type=int, help="The batch size for training.", default=8, nargs="?"
+)
 parser.add_argument("--seed", type=int, help="The seed.", default=327532, nargs="?")
-parser.add_argument("--output-dir", type=str, help="The directory to store results to", default="output_dir", nargs="?")
-parser.add_argument("--num-epochs", type=int, help="The number of training epochs", default="3", nargs="?")
+parser.add_argument(
+    "--output-dir",
+    type=str,
+    help="The directory to store results to",
+    default="output_dir",
+    nargs="?",
+)
+parser.add_argument(
+    "--num-epochs",
+    type=int,
+    help="The number of training epochs",
+    default="3",
+    nargs="?",
+)
 args = parser.parse_args()
 
 # Create the virtual environment if one does not already
-if not os.path.exists("venv"): 
+if not os.path.exists("venv"):
     create("venv", with_pip=True)
 
 # Install the requirements
 os.system("venv/bin/pip install -r requirements.txt")
-os.system("venv/bin/pip install torch==1.12.1+cu116 --extra-index-url https://download.pytorch.org/whl/cu116")
+os.system(
+    "venv/bin/pip install torch==1.12.1+cu116 --extra-index-url https://download.pytorch.org/whl/cu116"
+)
 
 command_args = [
     "venv/bin/python run_clm.py",
@@ -34,7 +52,7 @@ command_args = [
     "--overwrite_output_dir",
     "--logging_strategy epoch",
     "--logging_steps 1",
-    f"--num_train_epochs {args.num_epochs}"
+    f"--num_train_epochs {args.num_epochs}",
 ]
 
 command = " ".join(command_args)
