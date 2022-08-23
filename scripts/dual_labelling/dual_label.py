15c92bfe54d5c52798b00c0d4d055d2f6720a38c
Sam Dower <samdower@hotmail.co.uk>
2022-08-23 20:44:22 +0100

    dual_label.py now loads the specified added dataset

--- scripts/dual_labelling/dual_label.py
+++ scripts/dual_labelling/dual_label.py
@@ -1,4 +1,9 @@
 import argparse
+import os
+import tempfile
+import wandb
+import configparser
+import json
 
 # Parser to pass the run id through to the program
 parser = argparse.ArgumentParser(
@@ -11,4 +16,38 @@ parser.add_argument(
 )
 args = parser.parse_args()
 
-print(args.run_id)
+# Load the configuration
+config = configparser.ConfigParser()
+config.read("config.ini")
+
+# Initialise a run to retrieve this data
+run = wandb.init(
+    project=config["Wandb"]["Project"],
+    entity=config["Wandb"]["Entity"],
+    name="dual_labelling_access_run",
+)
+
+# use a temporary directory as an inbetween
+with tempfile.TemporaryDirectory() as tmpdirname:
+    # download the dataset into this directory from wandb
+    artifact_path_components = (
+        config["Wandb"]["Entity"],
+        config["Wandb"]["Project"],
+        args.run_id + ":latest",
+    )
+    artifact_path = "/".join(artifact_path_components)
+    artifact = run.use_artifact(
+        artifact_path,
+        type=config["Data Handling"]["DatasetType"],
+    )
+    artifact.download(tmpdirname)
+
+    # load dataset from this directory
+    file_path = os.path.join(tmpdirname, config["Data Handling"]["DatasetFileName"])
+
+    with open(file_path, "r") as file:
+        added_data = json.load(file)
+
+print(len(added_data["text"]))
+print(len(added_data["labels"]))
+print(len(added_data["ambiguities"]))
