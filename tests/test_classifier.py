bb5f781a80801b8a8d7092ed90ef7db19608ab4c
Sam Adam-Day <me@samadamday.com>
2022-09-04 14:34:14 +0200

    Testing to make sure HF classifiers always use uncertainty mixin

---
+++ tests/test_classifier.py
@@ -0,0 +1,7 @@
+from al_llm.classifier import HuggingFaceClassifier, UncertaintyMixin
+
+def test_subclassing():
+
+    # All Hugging Face classifiers must provide an uncertainty measure to 
+    # ensure that TokenByTokenSampleGenerator works
+    assert issubclass(HuggingFaceClassifier, UncertaintyMixin)
