24cd108973df5584241092c95ae9a9fcc8018d49
Sam Adam-Day <me@samadamday.com>
2022-09-06 13:22:15 +0200

    Added ability to run experiment over multiple seeds

--- scripts/run_experiment.py
+++ scripts/run_experiment.py
@@ -1,7 +1,7 @@
 from argparse import ArgumentParser, ArgumentDefaultsHelpFormatter
 
 from al_llm import Experiment, Parameters
-from al_llm.constants import WANDB_PROJECTS
+from al_llm.constants import WANDB_PROJECTS, EXPERIMENT_SEEDS
 
 # Default W&B run identification
 PROJECT_NAME = WANDB_PROJECTS["experiment"]
@@ -25,6 +25,13 @@ parser.add_argument(
     "--project-name", type=str, default=PROJECT_NAME, help="The W&B project to use."
 )
 
+# Add argument to run the experiment for multiple seeds
+parser.add_argument(
+    "--multiple-seeds",
+    action="store_true",
+    help="Run the experiment multiple times across different seeds",
+)
+
 # Add the parameters to the parser
 Parameters.add_to_arg_parser(parser, DEFAULTS)
 
@@ -34,13 +41,34 @@ cmd_args = parser.parse_args()
 # Get a Parameters object from these
 parameters = Parameters.from_argparse_namespace(cmd_args)
 
-# Make the experiment
-args = Experiment.make_experiment(
-    parameters=parameters,
-    run_id=cmd_args.run_id,
-    project_name=cmd_args.project_name,
-)
-experiment = Experiment(**args)
+# Set the seeds to work with
+if cmd_args.multiple_seeds:
+    seeds = EXPERIMENT_SEEDS
+else:
+    seeds = [EXPERIMENT_SEEDS[0]]
 
-# Run it
-experiment.run()
+for i, seed in enumerate(seeds):
+
+    print()
+    print()
+    print("=" * 79)
+    title = f"| EXPERIMENT: {i}  |  seed={seed}"
+    title += (" " * (78 - len(title))) + "|"
+    print(title)
+    print("=" * 79)
+    print()
+    print()
+
+    # Set the seed
+    parameters["seed"] = seed
+
+    # Make the experiment
+    args = Experiment.make_experiment(
+        parameters=parameters,
+        run_id=cmd_args.run_id,
+        project_name=cmd_args.project_name,
+    )
+    experiment = Experiment(**args)
+
+    # Run it
+    experiment.run()
