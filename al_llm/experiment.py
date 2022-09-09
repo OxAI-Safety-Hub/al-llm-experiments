ec6b5918078199679506ac4857d7f77176e1121c
Sam Adam-Day <me@samadamday.com>
2022-09-09 11:08:24 +0200

    Allowed using automatic labeller with pool sampler

--- al_llm/experiment.py
+++ al_llm/experiment.py
@@ -467,15 +467,15 @@ class Experiment:
             )
 
         # Set up the interface
-        if sg_model_name == "pool":
-            interface = PoolSimulatorInterface(parameters, dataset_container, wandb_run)
-        elif parameters["use_automatic_labeller"]:
+        if parameters["use_automatic_labeller"]:
             interface = AutomaticLabellerInterface(
                 parameters,
                 dataset_container,
                 wandb_run,
                 parameters["automatic_labeller_model_name"],
             )
+        elif sg_model_name == "pool":
+            interface = PoolSimulatorInterface(parameters, dataset_container, wandb_run)
         elif parameters["full_loop"]:
             interface = CLIInterface(parameters, dataset_container, wandb_run)
         else:
