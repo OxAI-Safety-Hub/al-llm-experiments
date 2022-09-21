a22000625bfb46925069192c7638675645e925dc
Sam Adam-Day <me@samadamday.com>
2022-09-21 14:17:19 +0100

    Put artifact constants in constants module

--- al_llm/constants.py
+++ al_llm/constants.py
@@ -29,3 +29,22 @@ CACHE_SIZE = "10GB"
 # The default tag for loading tapted models from W&B. Set to "latest" to always
 # load the latest model
 TAPTED_MODEL_DEFAULT_TAG = "default"
+
+# Saving and loading constants: Dataset Extensions
+DATASET_EXT_ARTIFACT_TYPE = "dataset-extension"
+DATASET_EXT_DATASET_FILE_NAME = "added_data.json"
+
+# Saving and loading constants: Classifier Models
+CLASSIFIER_ARTIFACT_TYPE = "classifier-model"
+CLASSIFIER_MODEL_FILE_NAME = "model_home.pt"
+
+# Saving and loading constants: Dual Labelling Results
+DUAL_LAB_ARTIFACT_TYPE = "dual-label-results"
+DUAL_LAB_LABELS_FILE_NAME = "labels.json"
+DUAL_LAB_RESULTS_FILE_NAME = "results.json"
+
+# Saving and loading constants: TAPT Models
+TAPT_PROJECT_NAME = "TAPT-Models"
+TAPT_ARTIFACT_TYPE = "TAPT-model"
+TAPT_MODEL_FILE_NAME = "model_home.pt"
+TAPT_PARAMETERS_FILE_NAME = "parameters_home.json"
