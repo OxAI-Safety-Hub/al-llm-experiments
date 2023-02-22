36e15967624be9084b783918c3aad1cab6b5fedb
CJ-Griffin <cj.griffin@protonmail.com>
2023-02-22 15:12:50 +0000

    changed Parameters to be a dataclass

--- tests/test_parameters.py
+++ tests/test_parameters.py
@@ -3,4 +3,4 @@ from al_llm.parameters import Parameters
 
 def test_parameter_constructor():
     # Create a parameters with mostly defaults.
-    parameters = Parameters(dataset_name="dummy", acquisition_function="dummy")
+    _ = Parameters(dataset_name="dummy", acquisition_function="dummy")
